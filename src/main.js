24eed951fa3c65a3c2f052f841eaa6fa6c3a4c99
julia sheremet and Susan Southall <susan@>
2018-04-26 11:38:38 -0700

    Add api method to scripts.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import { Recipes } from './scripts.js';
+import { Recipes } from './scripts.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,24 @@
-// export { Recipes };
+class Recipe() {
+  constructor() {
+
+  }
+
+  apiMethod() {
+    return new Promise(function(resolve, reject) {
+
+      let recipeRequest = new XMLHttpRequest();
+      let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeSearch}`;
+
+      recipeRequest.onload = function() {
+        if (this.status === 200) {
+          resolve(recipeRequest.response);
+        }else {
+          reject(Error(recipeRequest.statusText));
+        }
+      }
+      recipeRequest.open("GET", url, true);
+      recipeRequest.send();
+    });
+  }
+} // closing class
+ export { Recipes };
