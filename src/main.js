d23505d4d45c365d0b69a4fe4542abbc51e35081
julia sheremet and Susan Southall <susan@>
2018-04-26 11:16:54 -0700

    add initial file structure, html, and css for initial template

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import { Recipes } from './scripts.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function(){
+
+});
