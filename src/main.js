6f0da783c1f686147dc78ae85c68d008b837e81e
julia sheremet <juliajessicasher@gmail.com>
2018-04-26 21:39:14 -0700

    add second api call for getIngredients method

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-function getRecipes(meal) {
+function getIngredients(meal) {
   // $("#recipe").append("<p>" + response.meals[0].strMeal + response.meals[0].strArea + "</p>");
   // for(let key in response.meals) {
   //   if(key.includes(''))
@@ -13,14 +13,24 @@ function getRecipes(meal) {
   for (let key in meal) { //key is a placeholder name for response.meals[0] (called in the submit function)
     if ( (key.includes('strIngredient') && meal[key] !=null) && (key.includes('strIngredient') && meal[key] !='') ){                  //^value^
       //if ( (the key is true & the value is true) && (the key is true & the value is not an empty string))
-
-
-      $("ul#recipe").append('<li>' + meal[key] + '</li>');
+      $("ul#ingredients").append('<li>' + meal[key] + '</li>');
       mealsArray.push(meal[key]); //push the items in the array
     }
   }
   return mealsArray;
 }
+
+function getInstructions(meal) {
+  let instructionsArray = [];
+  for (let instruction in meal){
+    if (instruction.includes('strInstructions')){
+      $("#instructions").append("<strong><h2>" + "Instructions:" + "</h2></strong></br>" + meal[instruction]);
+      instructionsArray.push(meal[instruction]); //push the items in the array
+    }
+  }
+  return instructionsArray;
+}
+
 
 $(document).ready(function(){
   $("#submit-btn").click(function(){
@@ -37,7 +47,8 @@ $(document).ready(function(){
 
       response = JSON.parse(response);
       let meal = response.meals[0];
-      getRecipes(meal);
+      getIngredients(meal);
+      getInstructions(meal);
     }, function(Error){
       console.log("Sorry there is an Error loading your request!");
     });
